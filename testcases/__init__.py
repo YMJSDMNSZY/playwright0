5402f93223a5a5c1e6bd589600d6550b438c8a0a
YMJSDMNSZY <3281421386@qq.com>
2024-08-22 10:44:51 +0800

    61章-init文件优化  module内直接調用對應的類

--- testcases/__init__.py
+++ testcases/__init__.py
@@ -3,11 +3,11 @@ from playwright.sync_api import Page,expect,Browser,BrowserContext
 # from module.BaiduPage import Baidu
 # from module.登录页 import 登录页_类
 
-from module.PageInstance import PageIns
+from module import PageIns
 import pytest
 from filelock import FileLock
 from utils.GetPath import get_path
 from utils.globalMap import GlobalMap
 # from module.BasePage import PageObject,login_and_return_page_with_new_context
-from module import *
+
 import time
