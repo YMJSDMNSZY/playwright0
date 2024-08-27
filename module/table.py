50ac59bb148d5598d30c71457729ffdae426fe92
YMJSDMNSZY <3281421386@qq.com>
2024-08-27 14:13:47 +0800

    64章-通过表头的序列号以及某行的定位获取表格中某一格的数据

--- module/table.py
+++ module/table.py
@@ -13,3 +13,18 @@ class Table:
 
     def get_row_locator(self,行定位元素:Locator)->Locator:
         return self.table_div.locator("tr").filter(has=行定位元素)
+
+    def get_cell(self,表头文字or列序号,行元素定位or行序号or行文字)->Locator:
+        if isinstance(表头文字or列序号,str):
+            列序号 = self.get_header_index(表头文字or列序号)
+        else:
+            列序号=表头文字or列序号
+
+        if isinstance(行元素定位or行序号or行文字,Locator):
+            行定位 = self.get_row_locator(行元素定位or行序号or行文字)
+        elif isinstance(行元素定位or行序号or行文字,str):
+            行定位=self.table_div.locator("tr").filter(has_text=行元素定位or行序号or行文字)
+        else:
+            行定位 =self.table_div.locator("tbody").locator('//tr[not(@aria-hidden="true")]').nth(行元素定位or行序号or行文字)
+
+        return 行定位.locator("td").nth(列序号)
--- testcases/table_test.py
+++ testcases/table_test.py
@@ -3,6 +3,10 @@ from testcases import *
 def test_new_context(new_context):
     my_page_测试员=PageIns.login_and_return_page_with_new_context(new_context,"测试员")
     my_page_测试员.项目集.navigate()
-    my_page_测试员.项目集.主表格.get_header_index("项目集名称")
-    my_page_测试员.项目集.主表格.get_row_locator(my_page_测试员.page.get_by_text("自动化创建项目集_1724231092840667600")).highlight()
+    # index=my_page_测试员.项目集.主表格.get_header_index("开始时间")
+    # loc=my_page_测试员.项目集.主表格.get_row_locator(my_page_测试员.page.get_by_text("table_test"))
+    # my_page_测试员.项目集.主表格.get_cell("开始时间",my_page_测试员.page.get_by_text("table_test")).text_content()
+    # my_page_测试员.项目集.主表格.get_cell(1,"table_test").text_content()
+    # my_page_测试员.项目集.主表格.get_cell(1,-1).text_content()
+    my_page_测试员.项目集.主表格.get_cell(1,6).text_content()
     my_page_测试员
