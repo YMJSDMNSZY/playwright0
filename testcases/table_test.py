9468ffc28108f05419188cb34b355111b006910f
YMJSDMNSZY <3281421386@qq.com>
2024-09-05 13:12:22 +0800

    67章-表单的封装-数据类的创建

--- testcases/table_test.py
+++ testcases/table_test.py
@@ -12,5 +12,4 @@ def test_new_context(new_context):
     # my_page_测试员.项目集.主表格.get_row_dict(1, 6)
     #my_page_测试员.项目集.主表格.get_row_dict(my_page_测试员.page.get_by_text("自动化创建项目集_1724231150858667600"))
     my_page_测试员.项目集.主表格.get_col_dict("项目集名称")
-
     my_page_测试员
