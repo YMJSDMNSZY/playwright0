fbe4c8333662179b12807d847f4241708072d3a4
YMJSDMNSZY <3281421386@qq.com>
2024-09-10 14:40:14 +0800

    76章-表单的封装-数据类的使用

--- testcases/__init__.py
+++ testcases/__init__.py
@@ -9,5 +9,7 @@ from filelock import FileLock
 from utils.GetPath import get_path
 from utils.globalMap import GlobalMap
 # from module.BasePage import PageObject,login_and_return_page_with_new_context
+from data_module.项目集数据类模块 import 项目集数据类_新建项目集
+
 
 import time
---
+++ testcases/表单测试_使用数据类.py
@@ -0,0 +1,9 @@
+from testcases import *
+
+def test_new_context(new_context):
+    my_page_测试员=PageIns.login_and_return_page_with_new_context(new_context,"测试员")
+    my_page_测试员.项目集.navigate()
+    my_page_测试员.项目集.click_button("新建")
+    新建项目 = 项目集数据类_新建项目集(项目集名称="test123", 项目集周期="0,365", 父项目集="公共项目集")
+    my_page_测试员.项目集.快捷操作_填写表单(**新建项目.as_dict())
+    my_page_测试员
